16211398001e6dde790b05acb353b34955e84710
Vsrajput7773 <vishalsinghrajput5797@gmail.com>
2023-11-23 17:38:20 +0530

    fix the bug of database conection

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 //require ('dotenv').config({path:'./env'})
-import dotenv from "dotenv";
+import dotenv from "dotenv"
 import connectDB from "./db/index.js";
-// import {app} from './app.js'
+import {app} from './app.js'
 
 dotenv.config({
-    path:'./env'
+    path:'./.env'
 })
 
 connectDB()
 .then(()=>{
-    application.listen(process.env.PORT || 8000,()=>{
+    app.listen(process.env.PORT || 8000,()=>{
       console.log(` server is running at port :${process.env.PORT}`);
       })
     })
